875024b3e1436eeeb38c39bacccb64cbeab8d2de
Fede Otaran <otaran.federico@gmail.com>
2016-07-29 20:10:05 -0300

    Agregada la configuración básica para oauth.

--- src/main/java/com/pasae/PasaeApplication.java
+++ src/main/java/com/pasae/PasaeApplication.java
@@ -2,8 +2,16 @@ package com.pasae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RestController
 public class PasaeApplication {
 
 	public static void main(String[] args) {
