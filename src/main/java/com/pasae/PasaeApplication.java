4519c23e75ba5bb51940371f7ac3e0bfc49ce042
Fede Otaran <otaran.federico@gmail.com>
2016-08-07 13:30:50 -0300

    Se agrega configuración para que tome los usuarios desde el autorization server redefinido

--- src/main/java/com/pasae/PasaeApplication.java
+++ src/main/java/com/pasae/PasaeApplication.java
@@ -1,20 +1,85 @@
 package com.pasae;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RestController
 public class PasaeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PasaeApplication.class, args);
 	}
+	
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				// Just for laughs, apply OAuth protection to only 2 resources
+				.antMatcher("/**")
+				.authorizeRequests()
+				.anyRequest().access("#oauth2.hasScope('read')");
+			// @formatter:on
+		}
+
+		@Override
+		public void configure(ResourceServerSecurityConfigurer resources)
+				throws Exception {
+			resources.resourceId("pasae");
+		}
+
+	}
+	
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+		@Autowired
+		private AuthenticationManager authenticationManager;
+
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+				throws Exception {
+			endpoints.authenticationManager(authenticationManager);
+		}
+
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			// @formatter:off
+			clients.inMemory().withClient("my-trusted-client")
+					.authorizedGrantTypes("password", "authorization_code",
+							"refresh_token", "implicit")
+					.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+					.scopes("read", "write", "trust").resourceIds("pasae")
+					.accessTokenValiditySeconds(180).and()
+					.withClient("my-client-with-registered-redirect")
+					.authorizedGrantTypes("authorization_code").authorities("ROLE_CLIENT")
+					.scopes("read", "trust").resourceIds("pasae")
+					.redirectUris("http://anywhere?key=value").and()
+					.withClient("my-client-with-secret")
+					.authorizedGrantTypes("client_credentials", "password")
+					.authorities("ROLE_CLIENT").scopes("read").resourceIds("pasae")
+					.secret("secret");
+			// @formatter:on
+		}
+
+	}
+
+	
 }
