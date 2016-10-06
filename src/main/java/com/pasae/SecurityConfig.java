f302c28b6f6e2e4fcb6f4eb7e046791a78cf91b0
hchanf <hchanf@PC244P015W7BAL.UNINFO.UI.MECON.AR>
2016-10-06 09:38:34 -0300

    se activa config general de cors y se permite pasar a todos los request http options

---
+++ src/main/java/com/pasae/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.pasae;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+	}
+	
+}
